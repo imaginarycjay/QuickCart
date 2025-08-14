2f610684e28b02179b6ab8c00f0f3201b8e07d6c
imaginarycjay <imaginarycjay@gmail.com>
2025-08-14 17:20:38 +0800

    testing config

---
+++ resources/views/LandingPage.blade.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Online Karinderya</title>
+    {{-- init tailwind, di tayo gagamit ng bootstrap --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    {{-- temp content for testing --}}
+    <h1 class="text-center font-bold text-3xl mt-4">Online Kariderya MVP</h1>
+</body>
+</html>
