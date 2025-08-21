4d6d9143035da2ea6e44925e9818634e345aaf5d
imaginarycjay <imaginarycjay@gmail.com>
2025-08-21 23:23:42 +0800

    i

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,12 @@ use Illuminate\Support\Facades\Route;
 route::get('/', function () {
     return view('pages/userSide/landingPage');
 });
-Route::get('/about', function () {
+/* Route::get('/about', function () {
     return view('pages/aboutPage');
-});
-Route::get('/products', function () {
+}); */
+
+
+/* Route::get('/products', function () {
     return view('pages/productsPage');
-});
+}); */
 
