1e780657693bf6dc994f14db083625d75623f1ad
imaginarycjay <imaginarycjay@gmail.com>
2025-08-22 00:15:35 +0800

    hold on

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,6 +1,6 @@
 <html lang="en" class="h-full">
 
-<head>
+<head class="h-full">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -9,19 +9,6 @@
 </head>
 
 <body class="h-full bg-gray-100">
-{{-- 
-    <nav class=" flex justify-between ml-4">
-        <div class=" w-auto gap-3 flex">
-            <x-navlink href="/" :active="false">Home</x-navlink>
-            <x-navlink href="/products">Products</x-navlink>
-            <x-navlink href="/about">About</x-navlink>
-        </div>
-    </nav>
- --}}
-    <header>
-        {{ $header }}
-    </header>
-
     <main class="container mx-auto  mt-2"> 
         {{ $slot }}
     </main>
--- resources/views/pages/userSide/landingPage.blade.php
+++ resources/views/pages/userSide/landingPage.blade.php
@@ -1,7 +1,9 @@
 <x-layout title="Online Karinderya"> 
 
     <x-slot name="header">
-        <h1 class="font-bold text-3xl ml-5">A group 7 project</h1> 
+        <h1 class="font-bold text-3xl ml-5">A group 7 project</h1>
     </x-slot>
 
+    <h1>Hi this is the landing page</h1>
 </x-layout>
+
