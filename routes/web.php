54fdff0da821d3521ba1f691eadb4497411f0196
imaginarycjay <imaginarycjay@gmail.com>
2025-08-23 11:05:21 +0800

    conflicts

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,7 @@ use Illuminate\Support\Facades\Route;
 route::get('/', function () {
     return view('pages/userSide/landingPage');
 });
-<<<<<<< HEAD
 /* Route::get('/about', function () {
-=======
-
-Route::get('/about', function () {
->>>>>>> nav-feature
     return view('pages/aboutPage');
 }); */
 
